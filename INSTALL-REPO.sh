0e0bc7b29cc5d1aa000ee8b5efa82ea4e44c0421
Nicolás Gómez B. <nicolasg1911@gmail.com>
2024-05-09 10:53:05 -0500

    URL repo

--- INSTALL-REPO.sh
+++ INSTALL-REPO.sh
@@ -9,7 +9,7 @@ yum install -y /repos/yum.repos.d/ICINGA-release.repo
 yum install -y /repos/yum.repos.d/mongodb-org-44.repo
 yum install -y /repos/yum.repos.d/nmsprime-os.repo
 yum install -y /repos/yum.repos.d/nmsprime-prime.repo
-yum install -y /repos/yum.repos.d/pgdg-redhat-all.repo
+yum install -y /repos/yum.repos.d/pgdg-redhat-all.repo --disablerepo=pgdg-common
 yum install -y /repos/yum.repos.d/remi-safe.repo
 yum install -y /repos/yum.repos.d/timescale_timescaledb.repo
 # enable software collections, needed for rh-nodejs12
